814f3b8affa2c880fcddc01e9f7fb8d44119f282
Michael Alexander <michael_alexander_20@outlook.com>
2019-07-30 16:26:21 +0100

    Attempt 3 HW

---
+++ ruby_functions_practice.rb
@@ -0,0 +1,58 @@
+def return_10()
+  return 10
+end
+
+def add(num_1, num_2)
+  return num_1 + num_2
+end
+
+def subtract(num_1, num_2)
+  return num_1 - num_2
+end
+
+ def multiply(num_1, num_2)
+return num_1 * num_2
+ end
+
+def divide(num_1, num_2)
+  return num_1 / num_2
+end
+
+def length_of_string(string)
+  return string.length
+end
+
+def join_string(string_1 , string_2)
+  return string_1 + string_2
+end
+
+def add_string_as_number(string_1 , string_2)
+  return string_1.to_i + string_2.to_i
+end
+
+def number_to_full_month_name(number)
+when number = 1
+return "January"
+when number = 2
+return "February"
+when number = 3
+return "March"
+when number = 4
+return "April"
+when number = 5
+return "May"
+when number = 6
+return "June"
+when number = 7
+return "July"
+when number = 8
+return "August"
+when number = 9
+return "September"
+when number = 10
+return "October"
+when number = 11
+return "November"
+when number = 12
+return "December"
+end
